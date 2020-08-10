c249475c55225c580f6fc09a9ec1b133ba8a947f
AndreVTavares <andretavares3@gmail.com>
2020-08-10 20:43:23 -0300

    fix Classes Controller index whereRaw change from to to

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -16,8 +16,8 @@ export default class ClassesController {
     const week_day = filters.week_day as string;
     const time = filters.time as string;
 
-    if (!filters.week_day || filters.subject || filters.time) {
-      response.status(400).json({
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
         error: 'Missing filters search classes',
       });
     }
@@ -31,7 +31,7 @@ export default class ClassesController {
           .whereRaw('`class_schedule`.`class_id`= `classes`.`id`')
           .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
           .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`from` > ??', [timeInMinutes]);
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
       })
       .where('classes.subject', '=', subject)
       .join('users', 'classes.user_id', '=', 'users.id')
